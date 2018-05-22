dcaf743bc041a912db8adf6b873e89dc28ec8d75
Mark Paspirgilis <mark-paspirgilis@web.de>
2018-05-22 22:01:45 +0200

    analytics

--- components/js_includes.php
+++ components/js_includes.php
@@ -1,1 +1,13 @@
 <script src="assets/js/<?= ENV == 'live' ? 'min/' : '' ?>script.js" async></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119688651-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-119688651-2');
+</script>
