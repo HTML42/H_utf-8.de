a4145ac3353b98edc9aa7319e591b087886243c0
Mark Paspirgilis <mark-paspirgilis@web.de>
2018-05-22 23:22:06 +0200

    robots.txt

--- components/html_head.php
+++ components/html_head.php
@@ -2,6 +2,8 @@
 
 <title>UTF-8</title>
 
+<meta name="google-site-verification" content="RbBjx2LfLmac6949oYn9BQalwrqyf1N7g0VaSotLSAU" />
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <meta name="theme-color" content="#69D269"/>
